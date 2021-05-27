ed0fe74238240edcc2eb9259919afbc2336a91cb
ThoMich22 <thomas_8_69@hotmail.com>
2021-05-27 22:17:54 +0200

    fin

--- script_3.js
+++ script_3.js
@@ -4,20 +4,24 @@
 // Récupérer le nombre d'étages 
 const number_stage = prompt("Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?");
 
-
-
 let n = number_stage;
+
 let string = "";
+
 for (let i = 1; i <= n; i++) {
+  
   // printing spaces
   for (let j = 0; j < n - i; j++) {
     string += " ";
   }
-  // printing star
+
+  // printing #
   for (let k = 0; k < i; k++) {
-    string += "*";
+    string += "#";
   }
+
   string += "\n";
 }
+
 console.log(string);
 
--- script_6.js
+++ script_6.js
@@ -1,40 +1,41 @@
 arn = "CCGUCGUUGCGCUACAGC"
 arn_2 = "CCUCGCCGGUACUUCUCG"
 
-i = 1
+
 
 
 function transformation(arn) { 
 
     let x = arn.match(/.{1,3}/g);
+    let i = 0;
 
     x.forEach(element => {
-        if (element === "UCU" || "UCC" || "UCA" || "UCG" || "AGU" || "AGC"){
+        if (element === "UCU" || element === "UCC" || element === "UCA" || element === "UCG" || element === "AGU" || element === "AGC"){
 
-            x.splice(x[i], 1, 'Sérine')
+            x.splice(x[i], i, 'Sérine')
 
-        } else if (element === "CCU" || "CCC" || "CCA" || "CCG"){
+        } else if (element === "CCU" || element === "CCC" || element === "CCA" || element === "CCG"){
 
-            x.splice(x[i], 1, 'Proline')
+            x.splice(x[i], i, 'Proline')
 
-        } else if (element === "UUA" || "UUG"){
+        } else if (element === "UUA" || element === "UUG"){
 
-            x.splice(x[i], 1, 'Leucine')
+            x.splice(x[i], i, 'Leucine')
 
-        } else if (element === "UUU" || "UUC") {
+        } else if (element === "UUU" || element === "UUC") {
 
-            x.splice(x[i], 1, 'Phénylalanine')
+            x.splice(x[i], i, 'Phénylalanine')
 
-        } else if (element === "CGU" || "CGC" || "CGA" || "CGG" || "AGA" || "AGG") {
+        } else if (element === "CGU" || element === "CGC" || element === "CGA" || element === "CGG" || element === "AGA" || element === "AGG") {
 
-            x.splice(x[i], 1, 'Arginine')
+            x.splice(x[i], i, 'Arginine')
 
-        } else if (element === "UAU" || "UAC" )
+        } else if (element === "UAU" || element === "UAC" )
 
-            x.splice(x[i], 1, 'Tyrosine')
+            x.splice(x[i], i, 'Tyrosine')
     });
-    i++
-    return x 
+    i++;
+return x 
 };
 
 console.log(transformation(arn));
